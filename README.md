f0295be58907adf28951da86057ae0f09801f734
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-14 14:17:48 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,28 +1,8 @@
 # BinaryConvert
-  
+
 A super easy Python tool that converts your text into binary language (8-bit).
 
 ## Installation
 
 ```bash
 pip install binaryconvert
-```
-# Exmaple
-```python
-from binaryconvert.convert import binary
-
-binary("Hi, my name is wfxey!")
-```
-# Output
-```bash
-01001000
-01100001
-01100001
-01101100
-01100001
-01101100
-01101100
-01100001
-01100001
-01101111
-```
--- binaryconvert/convert.py
+++ binaryconvert/convert.py
@@ -1,7 +1,18 @@
-def binary(text):
+def ToBinary(text):
+    binary_list = []
     for char in text:
         binary_char = format(ord(char), '08b')
-        print(binary_char)
+        binary_list.append(binary_char)
+    return ' '.join(binary_list)
+
+def ToText(binary):
+    binary_values = binary.split()
+    ascii_characters = [chr(int(bv, 2)) for bv in binary_values]
+    return ''.join(ascii_characters)
 
 if __name__ == "__main__":
-    binary("Hi, my name is wfxey!")
+    binary_output = ToBinary("Hi, my name is wfxey!")
+    print(binary_output)
+    
+    text_output = ToText("01001000 01101001 00101100 00100000 01101101 01111001 00100000 01101110 01100001 01101101 01100101 00100000 01101001 01110011 00100000 01110111 01100110 01111000 01100101 01111001 00100001")
+    print(text_output)
