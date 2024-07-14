d1eddf1e996e7c3d39c9cf213044055d3b2d7939
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-14 13:34:16 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name='binaryconvert',
     version='1.0',
@@ -8,6 +11,9 @@ setup(
     description='A super easy Python tool that converts your text into binary language (8-bit).',
     author='wfxey',
     author_email='projectsdi02@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_username/binaryconvert",
     url='https://github.com/wfxey/binaryconvert',
     download_url='https://github.com/wfxey/binaryconvert/archive/refs/tags/v1.0.tar.gz',
     keywords=['binary', '8-bit', 'text-to-binary'],
